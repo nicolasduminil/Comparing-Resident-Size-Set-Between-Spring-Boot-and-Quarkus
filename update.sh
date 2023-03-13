8c84d5209cc752edf64dd1231f849deaf571e10b
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2023-03-13 16:55:42 +0100

    Have updated URL in update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,1 @@
-curl -X "PUT" "
-/update" -H "accept: */*" -H "Content-Type: application/json" -d "{\"name\": \"AWS Lambda\", \"author\": \"Nicolas DUMINIL\", \"publisher\": \"Manning\"}"
+curl -X "PUT" "http://localhost:8081/rss/update" -H "accept: */*" -H "Content-Type: application/json" -d "{\"name\": \"AWS Lambda\", \"author\": \"Nicolas DUMINIL\", \"publisher\": \"Manning\"}"
